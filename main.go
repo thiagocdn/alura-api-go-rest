eb66532446d225103c6b5e2be7c232b538dcd2fb
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-21 22:17:19 -0300

    Criado requisicao mockada

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/thiagocdn/alura-api-go-rest/models"
 	"github.com/thiagocdn/alura-api-go-rest/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Thiago", Historia: "Historia muito boa"},
+		{Nome: "Thiago 2", Historia: "Historia muito boa 2"},
+	}
+
 	fmt.Println("Iniciando servidor REST com Go...")
 	routes.HandleRequest()
 }
